7868a8802d83108a15b9c8e1ee4d7aa57e27eb5a
Juan Rivera <jrivera@primalplatoon.com>
2020-02-02 02:33:31 -0500

    Just need to properly save notes and re-render, and delete.  Aside from a few, easy minor styling enhancements, this app is just about done.

--- public/savedArticles.js
+++ public/savedArticles.js
@@ -8,10 +8,10 @@ $(document).ready(function () {
     // Adding event listeners for dynamically generated buttons for deleting articles,
     // pulling up article notes, saving article notes, and deleting article notes
     $(document).on("click", ".btn.del-article-btn", deleteArticle);
-    $(document).on("click", ".btn.notes-btn", articleNotes);
-    $(document).on("click", ".btn.save", handleNoteSave);
-    $(document).on("click", ".btn.note-delete", handleNoteDelete);
-    $(".clear").on("click", handleArticleClear);
+    $(document).on("click", "#notes-btn", myNotes);
+    $(document).on("click", "#savenote", saveMyNote);
+    // $(document).on("click", ".btn.note-delete", handleNoteDelete);
+    // $(".clear").on("click", handleArticleClear);
 
 
 
@@ -26,42 +26,14 @@ $(document).ready(function () {
                 $(".saved-articles").append("<section><h5 data-id='" +
                     data[i]._id + "'>" + data[i].title + "</h5>" + "<p class='story-summ'>" + data[i].summary + "<hr>" +
                     "<button type='button' class='btn btn-danger del-article-btn'>Remove Article</button>" +
-                    "<button type='button' class='btn btn-info notes-btn'>My Notes</button>" +
+                    "<button type='button' class='btn btn-info' id='notes-btn'>My Notes</button>" +
                     "</section></p>");
+
+
             }
         });
     }
 
-
-
-
-
-    function renderNotesList(data) {
-        // This function handles rendering note list items to our notes modal
-        // Setting up an array of notes to render after finished
-        // Also setting up a currentNote variable to temporarily store each note
-        var notesToRender = [];
-        var currentNote;
-        if (!data.notes.length) {
-            // If we have no notes, just display a message explaining this
-            currentNote = $("<li class='list-group-item'>No notes for this article yet.</li>");
-            notesToRender.push(currentNote);
-        } else {
-            // If we do have notes, go through each one
-            for (var i = 0; i < data.notes.length; i++) {
-                // Constructs an li element to contain our noteText and a delete button
-                currentNote = $("<li class='list-group-item note'>")
-                    .text(data.notes[i].noteText)
-                    .append($("<button class='btn btn-danger note-delete'>x</button>"));
-                // Store the note id on the delete button for easy access when trying to delete
-                currentNote.children("button").data("_id", data.notes[i]._id);
-                // Adding our currentNote to the notesToRender array
-                notesToRender.push(currentNote);
-            }
-        }
-        // Now append the notesToRender to the note-container inside the note modal
-        $(".note-container").append(notesToRender);
-    }
 
     function deleteArticle() {
         var section = $(this)
@@ -91,80 +63,85 @@ $(document).ready(function () {
             }
         });
     }
-    function articleNotes(event) {
-        // This function handles opening the notes modal and displaying our notes
-        // We grab the id of the article to get notes for from the card element the delete button sits inside
-        var currentArticle = $(this)
-            .parents("section")
-            .data();
-        // Grab any notes with this headline/article id
-        $.get("/api/notes/" + currentArticle._id).then(function (data) {
-            // Constructing our initial HTML to add to the notes modal
-            var modalText = $("<div class='container-fluid text-center'>").append(
-                $("<h4>").text("Notes For Article: " + currentArticle._id),
-                $("<hr>"),
-                $("<ul class='list-group note-container'>"),
-                $("<textarea placeholder='New Note' rows='4' cols='60'>"),
-                $("<button class='btn btn-success save'>Save Note</button>")
-            );
-            // Adding the formatted HTML to the note modal
-            bootbox.dialog({
-                message: modalText,
-                closeButton: true
-            });
-            var noteData = {
-                _id: currentArticle._id,
-                notes: data || []
-            };
-            // Adding some information about the article and article notes to the save button for easy access
-            // When trying to add a new note
-            $(".btn.save").data("article", noteData);
-            // renderNotesList will populate the actual note HTML inside of the modal we just created/opened
-            renderNotesList(noteData);
-        });
-    }
 
-    function handleNoteSave() {
-        // This function handles what happens when a user tries to save a new note for an article
-        // Setting a variable to hold some formatted data about our note,
-        // grabbing the note typed into the input box
-        var noteData;
-        var newNote = $(".bootbox-body textarea")
-            .val()
-            .trim();
-        // If we actually have data typed into the note input field, format it
-        // and post it to the "/api/notes" route and send the formatted noteData as well
-        if (newNote) {
-            noteData = { _headlineId: $(this).data("article")._id, noteText: newNote };
-            $.post("/api/notes", noteData).then(function () {
-                // When complete, close the modal
-                bootbox.hideAll();
-            });
-        }
-    }
 
-    function handleNoteDelete() {
-        // This function handles the deletion of notes
-        // First we grab the id of the note we want to delete
-        // We stored this data on the delete button when we created it
-        var noteToDelete = $(this).data("_id");
-        // Perform an DELETE request to "/api/notes/" with the id of the note we're deleting as a parameter
+    // Whenever someone clicks My Notes button
+    function myNotes() {
+        var section = $(this).parents("section")
+        const title = section.find('h5').html();
+        const link = section.find('a').attr('href');
+        const summary = section.find('p').html();
+        const thisId = section.find('h5').attr("data-id");
+
+        // Empty the notes from the note section
+        $("#article-notes").empty();
+
+        // Save the id associated with this button
+        // Now make an ajax call for the Article
         $.ajax({
-            url: "/api/notes/" + noteToDelete,
-            method: "DELETE"
-        }).then(function () {
-            // When done, hide the modal
-            bootbox.hideAll();
-        });
-    }
+            method: "GET",
+            url: "/saved-articles/",
+            data: {
+                saved: true,
+                title,
+                link,
+                summary
+            }
+        })
+            // With that done, add the note information to the page
+            .then(function (data) {
+                // console.log("==============================================================");
+                console.log("=======This is the 'id' being passed through: " + thisId);
+                // The title of the article
+                $("#article-notes").append("<h4>" + title + "</h4>");
+                // An input to enter a new title
+                $("#article-notes").append("<input id='titleinput' name='title' placeholder='Note Title'>");
+                // A textarea to add a new note body
+                $("#article-notes").append("<textarea id='bodyinput' name='body' placeholder='Memorable note here'></textarea>");
+                // A button to submit a new note, with the id of the article saved to it
+                $("#article-notes").append("<button class='btn btn-primary' data-id='" + thisId + "' id='savenote'>Save Note</button>");
 
-    function handleArticleClear() {
-        $.get("api/clear")
-            .then(function () {
-                savedArticles.empty();
-                loadPage();
+                // If there's a note in the article
+                if (data.note) {
+                    // Place the title of the note in the title input
+                    $("#titleinput").val(data.note.title);
+                    // Place the body of the note in the body textarea
+                    $("#bodyinput").val(data.note.body);
+                }
             });
     }
+
+    // When you click the savenote button
+    function saveMyNote() {
+        // Grab the id associated with the article from the submit button
+        var section = $(this).parents("section")
+        const thisId = section.find('h5').attr("data-id");
+
+
+        // Run a POST request to change the note, using what's entered in the inputs
+        $.ajax({
+            method: "POST",
+            url: "/saved-articles/" + thisId,
+            data: {
+                // Value taken from title input
+                title: $("#titleinput").val(),
+                // Value taken from note textarea
+                body: $("#bodyinput").val()
+            }
+        })
+            // With that done
+            .then(function (data) {
+                // Log the response
+                console.log(data);
+                // Empty the notes section
+                $("#article-notes").empty();
+            });
+
+        // Also, remove the values entered in the input and textarea for note entry
+        $("#titleinput").val("");
+        $("#bodyinput").val("");
+    };
+
 });
 
 
--- server.js
+++ server.js
@@ -130,14 +130,15 @@ app.get("/clearall", function (req, res) {
 
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function (req, res) {
+app.get("/saved-articles", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-    db.Article.findOne({ _id: req.params.id })
+    db.Savedarticle.findOne(req.body)
         // ..and populate all of the notes associated with it
         .populate("Note")
-        .then(function (dbArticle) {
+        .then(function (dbSavedarticle) {
             // If we were able to successfully find an Article with the given id, send it back to the client
-            res.json(dbArticle);
+            console.log("Here's the saved article, ie, dbSavedArticle: " + dbSavedarticle)
+            res.json(dbSavedarticle);
         })
         .catch(function (err) {
             // If an error occurred, send it to the client
@@ -147,18 +148,20 @@ app.get("/articles/:id", function (req, res) {
 
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function (req, res) {
+app.post("/saved-articles", function (req, res) {
+    console.log("------------> Here is the req.body of the Note being posted: " + req.body)
     // Create a new note and pass the req.body to the entry
     db.Note.create(req.body)
         .then(function (dbNote) {
+
             // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
             // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
             // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+            return db.Savedarticle.findOneAndUpdate({ _id: req.body }, { note: dbNote._id }, { new: true });
         })
-        .then(function (dbArticle) {
+        .then(function (dbSavedarticle) {
             // If we were able to successfully update an Article, send it back to the client
-            res.json(dbArticle);
+            res.json(dbSavedarticle);
         })
         .catch(function (err) {
             // If an error occurred, send it to the client
@@ -166,7 +169,7 @@ app.post("/articles/:id", function (req, res) {
         });
 });
 
-// POST route for saving a newly scraped article to the db 
+// POST route for saving a newly scraped article to the Savedarticles  
 app.post("/submit", function (req, res) {
     // Create a new Book in the database
     console.log("LINE 162: This is the app.post req.body from the server.js file: ", req.body);
@@ -182,7 +185,7 @@ app.post("/submit", function (req, res) {
     // }
 });
 
-// Route for getting saved scraped articles from the db
+// Route for getting saved scraped articles from db to render on Saved Articles page
 app.get("/saved-articles", function (req, res) {
     // Find all results from the scrapedData collection in the db
     db.Savedarticle.find({}, function (error, found) {
@@ -213,115 +216,7 @@ app.get("/delete", function (req, res) {
     });
 });
 
-//==========================================================================
-// ROUTES FOR NOTES CRUD functions
-//============================================================================
 
-
-// Simple index route
-app.get("/", function (req, res) {
-    res.send(index.html);
-});
-
-// 1. Save a note to the database's collection
-// POST: /submit
-// ===========================================
-app.post('/submit', function (req, res) {
-    console.log(req.body); // {}
-    db.Note.insert(req.body, (err, saved) => {
-        if (err) {
-            console.log(error);
-        } else {
-            res.send(saved);
-        }
-    })
-});
-// 2. Retrieve all notes from the database's collection
-// GET: /all
-// ====================================================
-app.get('/all', (req, res) => {
-    db.Note.find({}, (err, results) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.json(results);
-        }
-    })
-})
-// 3. Retrieve one note in the database's collection by it's ObjectId
-// TIP: when searching by an id, the id needs to be passed in
-// as (mongojs.ObjectId(IdYouWantToFind))
-// GET: /find/:id
-// ==================================================================
-app.get('/find/:id', (req, res) => {
-    const id = req.params.id;
-    db.Note.findOne({
-        _id: mongojs.ObjectId(id) // id = 12345 => ObjectId('12345')
-    }, (err, found) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.send(found)
-        }
-    })
-})
-// 4. Update one note in the database's collection by it's ObjectId
-// (remember, mongojs.ObjectId(IdYouWantToFind)
-// POST: /update/:id
-// ================================================================
-app.post('/update/:id', (req, res) => {
-    const id = req.params.id;
-    db.Note.update({
-        _id: mongojs.ObjectId(id)
-    }, {
-        $set: {
-            "title": req.body.title,
-            "note": req.body.note,
-            modified: Date.now()
-        }
-    }, (err, editedValue) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.send(editedValue);
-        }
-    })
-})
-// 5. Delete one note from the database's collection by it's ObjectId
-// (remember, mongojs.ObjectId(IdYouWantToFind)
-// GET: /delete/:id
-// ==================================================================
-app.get('/delete/:id', (req, res) => {
-    const id = req.params.id;
-    db.Note.remove({
-        _id: mongojs.ObjectId(id) // id = 12345 => ObjectId('12345')
-    }, (err, removed) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.send(removed)
-        }
-    })
-})
-// 6. Clear the entire note collection
-// GET: /clearall
-// ===================================
-app.get("/clearall", function (req, res) {
-    // Remove every note from the notes collection
-    db.Note.remove({}, function (error, response) {
-        // Log any errors to the console
-        if (error) {
-            console.log(error);
-            res.send(error);
-        }
-        else {
-            // Otherwise, send the mongojs response to the browser
-            // This will fire off the success function of the ajax request
-            console.log(response);
-            res.send(response);
-        }
-    });
-});
 // Listen on port 3000
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
